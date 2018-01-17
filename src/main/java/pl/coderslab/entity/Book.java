dceb597f41f6bea7c9f98e14de4becb910c209e6
Arkadiusz Józwiak <arkadiusz.jozwiak@coderslab.pl>
2018-01-17 17:38:53 +0100

    fix indentation

--- src/main/java/pl/coderslab/entity/Book.java
+++ src/main/java/pl/coderslab/entity/Book.java
@@ -3,7 +3,6 @@ package pl.coderslab.entity;
 import java.util.List;
 
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
